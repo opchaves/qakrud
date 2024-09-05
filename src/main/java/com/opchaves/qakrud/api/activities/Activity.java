0baaa6b18834c95e3d0e8ebc4ccb80984107d02d
opchaves <opaulochaves@gmail.com>
2024-09-05 12:26:29 -0300

    rename sequent and increment id by 1

--- src/main/java/com/opchaves/qakrud/api/activities/Activity.java
+++ src/main/java/com/opchaves/qakrud/api/activities/Activity.java
@@ -9,18 +9,27 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
-import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
 @Entity(name = "activities")
 @Schema(name = "activity", description = "The activity resource")
-public class Activity extends PanacheEntity {
+public class Activity extends PanacheEntityBase {
+
+  @Id
+  @SequenceGenerator(name = "activities_id_seq", sequenceName = "activities_id_seq", initialValue = 1, allocationSize = 1)
+  @GeneratedValue(generator = "activities_id_seq", strategy = GenerationType.SEQUENCE)
+  public Long id;
 
   @NotBlank
   @Length(min = 3)
