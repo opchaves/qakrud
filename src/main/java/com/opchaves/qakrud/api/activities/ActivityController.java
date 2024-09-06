f6ed7a851523e07b6dcafb3f5bbeee4af7309aa2
opchaves <opaulochaves@gmail.com>
2024-09-06 08:44:05 -0300

    add examples

--- src/main/java/com/opchaves/qakrud/api/activities/ActivityController.java
+++ src/main/java/com/opchaves/qakrud/api/activities/ActivityController.java
@@ -2,23 +2,32 @@ package com.opchaves.qakrud.api.activities;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.HttpHeaders;
 
 @Path("/api/activities")
 @Tag(name = "activities")
@@ -47,5 +56,27 @@ public class ActivityController {
 
     return category.map(this.activityService::findByCategory).orElseGet(activityService::findAll)
         .invoke(activities -> Log.debugf("Total activities found = %d", activities.size()));
+  }
+
+  @POST
+  @Consumes(APPLICATION_JSON)
+  @Operation(summary = "Create an activity")
+  @APIResponse(
+    responseCode = "201",
+    description = "The URI of the created activity",
+    headers = @Header(name = HttpHeaders.LOCATION, schema = @Schema(implementation = URI.class)))
+  @APIResponse(
+    responseCode = "400",
+    description = "Invalid activity passed in (or no request body found)")
+  public Uni<Activity> createActivity(@RequestBody(
+    name = "activity",
+    required = true,
+    content = @Content(
+      mediaType = APPLICATION_JSON,
+      schema = @Schema(implementation = Activity.class),
+      examples = @ExampleObject(
+        name = "valid_activity",
+        value = Examples.VALID_CREATE_ACTIVITY_JSON))) @Valid @NotNull Activity activity) {
+    return activityService.persistActivity(activity);
   }
 }
---
+++ src/main/java/com/opchaves/qakrud/api/activities/Examples.java
@@ -0,0 +1,33 @@
+package com.opchaves.qakrud.api.activities;
+
+final class Examples {
+  private Examples() {
+  }
+
+  static final String VALID_CREATE_ACTIVITY_JSON = """
+      {
+        "title": "Pizza",
+        "note": "I love pizza",
+        "paid": true,
+        "price": 10.00,
+        "category": "Food"
+      }
+      """;
+
+  static final String VALID_ACTIVITY_JSON = """
+      {
+        "id": 1,
+        "title": "Pizza",
+        "note": "I love pizza",
+        "paid": true,
+        "price": 10.00,
+        "category": "Food",
+        "type": "expense",
+        "handledAt": "2024-08-01T00:00:00Z",
+        "createdAt": "2024-08-01T00:00:00Z",
+        "updatedAt": "2024-08-01T00:00:00Z",
+      }
+      """;
+
+  static final String VALID_LIST_ACTIVITIES_JSON = "[" + VALID_ACTIVITY_JSON + "]";
+}
